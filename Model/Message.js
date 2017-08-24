8225695983d5438ecbdfc8971764c99e7864c172
Brenno Dantas <brenno.dantas@driveonauto.com>
2017-08-24 09:58:29 -0400

    Mudando a tabela do banco

--- Model/Message.js
+++ Model/Message.js
@@ -68,6 +68,6 @@ var MessageSchema = new Schema({
 })
 
 //Register schema to Mongoose
-var Message = mongoose.model('Message_temp', MessageSchema);
+var Message = mongoose.model('Message', MessageSchema);
 
 module.exports = Message
