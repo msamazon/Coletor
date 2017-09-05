fc8ca73694edb0378bc56534cf0b15e596c315c4
Brenno Dantas <brenno.dantas@driveonauto.com>
2017-09-05 13:13:46 -0400

    byte do gps

--- Util/gpsConvert.js
+++ Util/gpsConvert.js
@@ -59,18 +59,21 @@ exports.calcule = function(gpshex) {
     
     if (bit2 ==  "1") { 
         console.log("N %",bit2)
-        bit2 = ""
+        bit2 = "+"
     }else {
-        bit2 = "-"
         console.log("S %",bit2)
+        bit2 = "-"
+        
     }
     
-    if (bit3 ==  "1") { 
-        bit3 = ""
+    if (bit3 ==  "1") {
         console.log("L %",bit3)
+        bit3 = "+"
+        
     }else {
-        bit3 = "-"
         console.log("O %",bit3)
+        bit3 = "-"
+        
     }
 
         //lat e long
