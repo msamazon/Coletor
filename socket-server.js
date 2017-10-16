808e5cbd96deab313859a786e82c67eb4f79ed03
Brenno Dantas <brenno.dantas@driveonauto.com>
2017-10-16 19:47:07 -0200

    mudando porta

--- socket-server.js
+++ socket-server.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose')
 var Handler = require('./handler')
 var server = net.createServer()
 
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 3000
+
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://dbuser:123mudar#@ds139428.mlab.com:39428/driveondb', { useMongoClient: true })
