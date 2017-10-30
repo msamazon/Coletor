a0c18a4a1cadb1fd8e8258312390b9e5e65895fe
Brenno <brenno.dantas@driveonauto.com>
2017-10-30 14:59:41 -0200

    log

--- Util/Cerberus.js
+++ Util/Cerberus.js
@@ -2,24 +2,34 @@
  * findDeviceId - 'e uma função que verifica se o donglecode esta na white list (Device)
  */
 
-exports.whitelist = function(dongleCode) {
+exports.whitelist = function(code) {
 
     var mongoose        = require("mongoose")
     var Device           = require("../Model/Device")
     
     var result = false
 
-    console.log("Cerberus::dongle %s", dongleCode)
+    console.log("Cerberus::dongle %s", code)
     
-    Device.find({"dongleCode": dongleCode}).exec(function (err, device) {
+    Device.find({"dongleCode": code}).exec(function (err, device) {
 
-        if (device.dongleCode == device) {
+        console.log("Cerberus::code %s", code)
+
+        console.log("Cerberus::dongleCode %s", device.dongleCode)
+
+        if (device.dongleCode == code) {
+
             result = true
+            console.log.apply("|||||||||||||||| %s", result)
             
         }else {
             result = false
+            console.log.apply("|||||||||||||||| %s", result)
         }
     })
+
+    console.log("Cerberus::result %s", result)
+
     return result
 }    
 
