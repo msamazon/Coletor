5a2683c6234642b51549bca7cb3e3826c7aabb31
Brenno Dantas <brenno.dantas@driveonauto.com>
2017-11-16 21:30:46 -0400

    novo config

---
+++ Util/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+    "database_mlab": "mongodb://dbuser:123mudar#@ds139428.mlab.com:39428/driveondb",
+    "database" : "mongodb://127.0.0.1:27017/driveondb",
+    "port": process.env.PORT || 4883,    
+}
